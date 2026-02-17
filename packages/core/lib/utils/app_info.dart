6e894b6aba85270c2461622700bfa16554feb284
aksbyte <theaksbyte@gmail.com>
2026-02-17 17:49:05 +0530

    packages added shared for user and caller

---
+++ packages/core/lib/utils/app_info.dart
@@ -0,0 +1,35 @@
+import 'package:core/constants/app_colors.dart';
+import 'package:core/constants/app_strings.dart';
+import 'package:flutter/material.dart';
+
+class AppInfo extends StatelessWidget {
+  final bool showVersion;
+  final bool isVisible;
+
+  const AppInfo({super.key, this.showVersion = true, this.isVisible = true});
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isVisible) return const SizedBox.shrink();
+
+    return  Material(
+      color: Colors.transparent,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "${AppStrings.appName} ${AppStrings.appVersion}",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.darkGrey,
+                fontSize: 12,
+                letterSpacing: 0.5,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
